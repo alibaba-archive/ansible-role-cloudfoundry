a44e5842c22e5dbd8d3e1b13edcb3c638589c3b6
He Guimin <guimin.hgm@alibaba-inc.com>
2017-11-15 16:36:07 +0800

    remove useless code

--- roles/cf-deploy/files/convert.sh
+++ roles/cf-deploy/files/convert.sh
@@ -22,21 +22,15 @@ do
         STEMCELLS=true
     fi
 
-    if [[ $LINE == *version:* ]]; then
-#        if [[ ${RELEASES} == true ]]; then
-#            echo $LINE | sed 's/version: .*/version: latest/g'  >> $CF_DEPLOYMENT
-#        fi
-
-        if [[ ${STEMCELLS} == true ]]; then
+    if [[ ${STEMCELLS} == true ]]; then
+        if [[ $LINE == *version:* ]]; then
             echo "  name: bosh-alicloud-kvm-ubuntu-trusty-go_agent" >> $CF_DEPLOYMENT
             echo $LINE | sed "s/version: .*/version: $2/g"  >> $CF_DEPLOYMENT
+        elif [[ $LINE == *os:* ]]; then
+            continue
+        else
+            echo $LINE >> $CF_DEPLOYMENT
         fi
-    elif [[ $LINE == *os:* && ${STEMCELLS} == true ]]; then
-        continue
-#    elif [[ $LINE == *url:* && ${RELEASES} == true* ]]; then
-#        continue
-#    elif [[ $LINE == *sha1:* && ${RELEASES} == true* ]]; then
-#        continue
     else
         echo $LINE >> $CF_DEPLOYMENT
     fi
